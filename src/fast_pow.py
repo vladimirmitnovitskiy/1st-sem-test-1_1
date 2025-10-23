50642d08eb68a58acb551cb917b468b3c75974e4
vladimirmitnovitskiy <harrellprmime@gmail.com>
2025-10-23 14:46:58 +0300

    исправлена ошибка из-за которой выводились не верные ответы, теперь работает с  отрицательными значениями power

--- src/fast_pow.py
+++ src/fast_pow.py
@@ -1,6 +1,15 @@
 def fastPow(number, power):
-    result = number
-    while power != 1:
-        result *= result
+    result = 1
+    flag = 0
+    if power < 0:
+        power = -power
+        flag = 1
+    while power > 0:
+        if power % 2 == 1:
+            result *= number
+        number *= number
         power = power // 2
+    if flag == 1:
+        return (f"1/{result}")
     return result
+
